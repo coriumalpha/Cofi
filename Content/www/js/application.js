444e1fe509e65745239bb1d37e6ac05c2cebd16d
coriumalpha <itsasontsi@gmail.com>
2016-02-11 23:14:08 +0100

    New server (2nd APP Deploy)

---
+++ Content/www/js/application.js
@@ -0,0 +1,117 @@
+
+
+$(document).on('pageinit', '#login', function()
+{  
+    $(document).on('click', '#submit', function()
+    { 
+        if($('#username').val().length > 0 && $('#password').val().length > 0)
+        {
+                $.ajax({url: 'http://raspi.hol.es/api.php',
+                    data: {action : 'login', formData : $('#loginForm').serialize()},
+                    type: 'post',                   
+                    async: 'true',
+                    dataType: 'json',
+                    success: function (result) {
+                        if(result.status) {
+                            $.mobile.changePage("#siteList");                         
+                        } else {
+                            alert(result.message); 
+                        }
+                    },
+                    error: function (request,error) {          
+                        alert('Error de red/servidor.');
+                    }
+                });                   
+        } else {
+            alert('Campos vacíos');
+        }           
+        return false; 
+    });    
+});
+
+$(document).on('pageinit', '#insert', function()
+{  
+    $(document).on('click', '#submit', function()
+    { 
+        if($('#name').val().length > 0 && $('#essid').val().length > 0 && $('#wifiPass').val().length > 0 && $('#location').val().length > 0)
+        {
+                $.ajax({url: 'http://raspi.hol.es/api.php',
+                    data: {action : 'insert', formData : $('#insertForm').serialize()},
+                    type: 'post',                   
+                    async: 'true',
+                    dataType: 'json',
+                    success: function (result) {
+                        if(result.status) {
+                            loadListView();
+                            $.mobile.changePage("#siteList");
+                        } else {
+                            alert(result.message); 
+                        }
+                    },
+                    error: function (request,error) {          
+                        alert('Error de red/servidor.');
+                    }
+                });                   
+        } else {
+            alert('Campos vacíos');
+        }           
+        return false; 
+    });    
+});
+
+
+function loadListView()
+{
+    $.ajax({url: 'http://raspi.hol.es/api.php',
+        data: {action : 'showList',},
+        type: 'post',                   
+        async: 'true',
+        dataType: 'json',
+        success: function (result) {
+                $('#listView').html("");
+            for (var i = 0; i < result.length; i++) {
+                $('#listView').append(
+                    '<li><a href="#siteDetails?id=' +result[i].barId+ '"><i class="' +result[i].plugs+ '"></i>  '+result[i].nombre+
+                    '<p><b>' +result[i].essid+ '</b> : '  +result[i].wifiPass+ 
+                    '</p><p>' +result[i].location+ '</p></a>' +
+                    '</li>'
+                    );
+            }
+            $('#listView').listview('refresh');              
+        },
+        error: function (request,error) {          
+            alert('Error de red/servidor.');
+        }
+    });
+}
+
+
+$(document).on('pageinit', '#siteList', function()
+{
+    loadListView();
+});
+
+
+function closeSession()
+{
+    $.ajax({url: 'http://raspi.hol.es/api.php',
+        data: {action : 'logout',},
+        type: 'post',                   
+        async: 'true',
+        dataType: 'json',
+        success: function (result) {
+            if(result.status)
+            {
+                alert("Sesión destruída.");
+                $.mobile.changePage("#login");
+            }
+            else
+            {
+                alert("Error al destruir sesión.");
+            }           
+        },
+        error: function (request,error) {          
+            alert('Error de red/servidor.');
+        }
+    });
+}
---
+++ README.md
@@ -0,0 +1,34 @@
+# Wiffee
+
+![alt text][logo]
+[logo]: https://github.com/coriumalpha/wiffee/blob/master/Content/www/images/miniLogo.png "Logo Title"
+
+### Personal WiFi AP key database gestor.
+
+
+***
+
+
+### Project core files
+
+* [(Spanish) Resumen del proyecto y estructura de BBDD]([LÉEME]_Resumen_Del_Proyecto_Wiffee.pdf)
+
+* [Main Application - index.html](Content/www/index.html)
+
+* [JS Side - application.js](Content/www/js/application.js)
+
+* [Images (Photoshoped Logo)](Content/www/images/)
+
+* [Serverside - API](Content/servidor/api.php)
+
+* [Config file](Content/config.xml)
+
+
+***
+
+
+### Test web server
+####(It may not be online)
+
+[Web server](http://raspi.hol.es/)
+
